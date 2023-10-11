d8d2f2bbbf140fb7ae1eaa0f8d6c74400e5cdb64
Jaya Chandra Mellamarthi <mjchandra97@gmail.com>
2023-10-11 20:04:37 +0530

    Added database connection to the server and created the basic model-schema for a to-do item

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const PORT = 8080; 
+const PORT = 8080;
+const DB_NAME = "TODO-DB";
+const MONGODB_URL = "mongodb://127.0.0.1:27017/" + DB_NAME;
 
 const app = express();
 
-app.use('/', (req,res,next)=>{
-    res.status(200).json({
-        message:"Hello from root path '/'"
-    });
-})
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Hello from root path '/'",
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server started on ${PORT}`);
-})
+mongoose
+  .connect(MONGODB_URL)
+  .then((res) => {
+    console.log(`App connected to the mongodb successfully to Database: ${DB_NAME}`);
+    app.listen(PORT, () => {
+      console.log(`Node Express server started on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Error occurred while connecting to Mongodb! : ${err}`);
+  });
---
+++ models/toDo.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const toDoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const todoModel = mongoose.model("ToDo", toDoSchema);
+
+module.exports = todoModel;
