cd65b17331b88ad396c74cee956431be03bf5989
Jaya Chandra Mellamarthi <mjchandra97@gmail.com>
2023-10-12 20:25:10 +0530

    Added the routes for fetching all todo items and fetch a single todo item by it's id.

--- controllers/todo.js
+++ controllers/todo.js
@@ -3,15 +3,15 @@ const { body, validationResult } = require("express-validator");
 // import Model
 const Todo = require("../models/toDo");
 
+// create a new todo item
 exports.createTodo = async (req, res, next) => {
   console.log("POST /todo reached");
   console.log(`Request body: ${req.body}`);
   // get any validation errors from the request
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error("Validation failed for the to-do item.");
     return res.status(422).json({
-      message: "Error occurred",
+      message: "Error occurred. Validation failed for the to-do item.",
       errors: errors.array(),
     });
   }
@@ -21,17 +21,74 @@ exports.createTodo = async (req, res, next) => {
       title: req.body.title,
     });
     await todo.save();
-    res.status(201).send({
+    res.status(201).json({
       message: "Created the To-do successfully",
       todo: {
         id: todo._id,
       },
     });
-  } catch (error) {
+  } catch (err) {
     // server error occur
     res.status(500).json({
       message: "Error occurred while creating todo item",
-      errors: error,
+      errors: err,
     });
   }
 };
+
+// fetches all the to-dos in the db
+exports.fetchAllTodo = async (req, res, next) => {
+  console.log("GET /todo/all reached");
+  try {
+    const toDos = await Todo.find({});
+    console.log(`Total number of to-dos found: ${toDos.length}`);
+    res.status(200).json({
+      message: "To-dos successfully fetched",
+      todoItems: {
+        total: toDos.length,
+        items: [...toDos],
+      },
+    });
+  } catch (err) {
+    // server error occur
+    res.status(500).json({
+      message: "Error occurred while fetching todo items",
+      errors: err.message,
+    });
+  }
+};
+
+// fetch a todo item by it's id
+exports.fetchTodoById = async (req, res, next) => {
+  console.log("GET /todo/:id reached");
+  console.log(`Request params: id:${req.params.id}`);
+  // get any validation errors from the request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Error occurred. Validation failed for the to-do item.",
+      errors: errors.array(),
+    });
+  }
+  try {
+    const id = req.params.id;
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({
+        message: `Todo with id: ${id} not found`,
+      });
+    }
+    res.status(200).json({
+      message: "Todo found successfully!",
+      todo: todo,
+    });
+  } catch (err) {
+    // server error occur
+    res.status(500).json({
+      message: "Error occurred while fetching todo item",
+      errors: err,
+    });
+  }
+};
+
+
--- routes/todo.js
+++ routes/todo.js
@@ -1,16 +1,33 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
+const { body, param } = require("express-validator");
 const todoRouter = express.Router();
 
 // import controllers
 const todoController = require("../controllers/todo");
 
 // Create a new todo item
-// POST '/todo/new' 
+// POST '/todo/new'
 todoRouter.post(
   "/new",
   [body("title").trim().not().isEmpty().withMessage(`Todo tile is invalid`)],
   todoController.createTodo
 );
+
+// Route to fetch the todo item by it's id (_id)
+// GET 'todo/:id'
+todoRouter.get(
+  "/all",
+  todoController.fetchAllTodo
+);
+
+// Route to fetch the todo item by it's id (_id)
+// GET 'todo/:id'
+todoRouter.get(
+  "/:id",
+  [param("id").trim().not().isEmpty().withMessage("Todo id is not available")],
+  todoController.fetchTodoById
+);
+
+
 
 module.exports = todoRouter;
